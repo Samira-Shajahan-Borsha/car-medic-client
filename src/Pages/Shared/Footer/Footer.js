a19b26dfe3016b7b7a6ff9e553645adf8a3179e9
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-25 09:04:38 +0600

    styled banner

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,30 +3,27 @@ import logo from '../../../assets/logo.svg';
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-32 bg-black text-white">
             <div>
                 <img src={logo} alt="" />
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <p>Edwin Diaz is a software <br /> and web technologies engineer, <br /> a life coach trainer who is also a serial .</p>
             </div>
             <div>
-                <span className="footer-title">Services</span>
-                <a href="/" className="link link-hover">Branding</a>
-                <a href="/" className="link link-hover">Design</a>
-                <a href="/" className="link link-hover">Marketing</a>
-                <a href="/" className="link link-hover">Advertisement</a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a href="/" className="link link-hover">About us</a>
+                <span className="footer-title">About</span>
+                <a href="/" className="link link-hover">Home</a>
+                <a href="/" className="link link-hover">Service</a>
                 <a href="/" className="link link-hover">Contact</a>
-                <a href="/" className="link link-hover">Jobs</a>
-                <a href="/" className="link link-hover">Press kit</a>
             </div>
             <div>
-                <span className="footer-title">Legal</span>
-                <a href="/" className="link link-hover">Terms of use</a>
-                <a href="/" className="link link-hover">Privacy policy</a>
-                <a href="/" className="link link-hover">Cookie policy</a>
+                <span className="footer-title">COMPANY</span>
+                <a href="/" className="link link-hover">Why Car Medic</a>
+                <a href="/" className="link link-hover">About</a>
+            </div>
+            <div>
+                <span className="footer-title">SUPPORT</span>
+                <a href="/" className="link link-hover">Support Center</a>
+                <a href="/" className="link link-hover">Feedback</a>
+                <a href="/" className="link link-hover">Accesbility</a>
             </div>
         </footer>
     );
