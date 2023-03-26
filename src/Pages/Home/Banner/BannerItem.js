b39d50a831943b86e6dc0f0f2b3fa0ed6c2a9b08
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-27 03:18:53 +0600

    reponsive banner implemented

--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -8,21 +8,21 @@ const BannerItem = ({ slide }) => {
             <div className='carousel-image'>
                 <img src={image} className="w-full rounded-xl" alt="" />
             </div>
-            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
-                <h1 className='text-6xl text-white font-bold'>
+            <div className="carousel-info absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                <h1 className='text-xl text-white font-bold md:text-4xl lg:text-6xl'>
                     Affordable <br />
                     Price For Car <br />
                     Servicing
                 </h1>
             </div>
             <div className="absolute flex justify-end w-2/5 transform -translate-y-1/2 left-24 top-1/2">
-                <p className='text-xl text-white'>
+                <p className='text-xl text-white hidden lg:block md:block'>
                     There are many variations of passages of  available, but the majority have suffered alteration in some form
                 </p>
             </div>
             <div className="absolute flex justify-start w-2/5 transform -translate-y-1/2 left-24 top-3/4">
                 <button className="btn border-0 bg-orange-600 hover:bg-orange-600 normal-case mr-5">Discover More</button>
-                <button className="btn btn-outline btn-warning">Latest Project</button>
+                <button className="btn btn-outline btn-warning hidden md:block">Latest Project</button>
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                 <a href={`#slide${prev}`} className="btn btn-circle mr-5 hover:bg-amber-500 border-0">❮</a>
