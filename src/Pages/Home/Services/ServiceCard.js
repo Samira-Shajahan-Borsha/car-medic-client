123e8b346ac8877dd748d372c8684c8cd425d9cc
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-29 02:55:12 +0600

    loaded all services data from backend

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
-    const { title, img, price } = service;
+    const { _id, title, img, price } = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl h-96">
             <div className='p-6'>
@@ -10,8 +11,10 @@ const ServiceCard = ({ service }) => {
                 <div className="card-body mt-5">
                     <h2 className="card-title">{title}</h2>
                     <div className="card-actions justify-end ">
-                    <p className='text-xl text-orange-600 font-medium'>Price: ${price}</p>
-                        <button className='text-orange-600 w-4'><FaArrowRight className='text-2xl'></FaArrowRight></button>
+                        <p className='text-xl text-orange-600 font-medium'>Price: ${price}</p>
+                        <Link to={`/checkout/${_id}`}>
+                            <button className='text-orange-600 w-4'><FaArrowRight className='text-2xl'></FaArrowRight></button>
+                        </Link>
                     </div>
                 </div>
             </div>
