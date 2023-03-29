37894b27fe139c5f9b62386270f8432073aad87d
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-30 00:45:34 +0600

    displayed orders

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
 
     const { user } = useContext(AuthContext);
 
     const [orders, setOrders] = useState([]);
-
 
     useEffect(() => {
         fetch(`http://localhost:5000/orders?email=${user?.email}`)
@@ -17,6 +17,27 @@ const Orders = () => {
     return (
         <div>
             <h2 className='text-5xl'>You have {orders.length} orders</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow
+                                key={order._id}
+                                order={order}
+                            ></OrderRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
